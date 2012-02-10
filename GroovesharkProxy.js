ca7b5e276412cbce4524c3dceb2077b5ee71f020
Michael Feldstein <feldstein@google.com>
2012-02-10 01:25:52 -0800

    Add some comments

--- GroovesharkProxy.js
+++ GroovesharkProxy.js
@@ -1,9 +1,11 @@
-var script = document.createElement('script');
 var GroovesharkProxyDiv = document.createElement('div');
 GroovesharkProxyDiv.id = 'GroovesharkProxyDiv';
 GroovesharkProxyDiv.style.display = 'none';
 document.body.appendChild(GroovesharkProxyDiv);
+
+// This is the function that will be injected into the page via a script tag.
 function runOnPage() {
+    // Cache the get values every time the song status changes so it can be pulled from the proxy.
     var GSGetMethods = [
         'getAPIVersion',
         'getApplicationVersion',
@@ -20,7 +22,11 @@ function runOnPage() {
         }
         GroovesharkProxyDiv.setAttribute('data-gs-get-values', JSON.stringify(values));
     }
+
     var GroovesharkProxyDiv = document.getElementById('GroovesharkProxyDiv');
+
+    // Always use this callback in the actual page so we can cache the values, and send it to
+    // any possible callback set in the extension.
     function songStatusCallback(data) {
         cacheGetValues();
         GroovesharkProxyDiv.innerText = JSON.stringify(data);
@@ -28,7 +34,10 @@ function runOnPage() {
         evt.initEvent("GroovesharkSongEvent", true, true );
         GroovesharkProxyDiv.dispatchEvent(evt);
     }
-    
+
+    // Listen for events sent to the page from the extension, and rn them with any
+    // arguments stored in the attributes.  Cache the get values afterwards in case
+    // anything changed.
     GroovesharkProxyDiv.addEventListener('GroovesharkSendAction', function() {
         var f = window.Grooveshark[GroovesharkProxyDiv.getAttribute('data-send-action')]
         var args = JSON.parse(GroovesharkProxyDiv.getAttribute('data-send-action-arguments'));
@@ -37,10 +46,15 @@ function runOnPage() {
     });
     window.Grooveshark.setSongStatusCallback(songStatusCallback);
 }
+
+// Append and run the script.
+var script = document.createElement('script');
 script.innerHTML = runOnPage + "\nrunOnPage();";
 document.body.appendChild(script);
 
 var GroovesharkProxy = {};
+// Call a method on the Grooveshark object in the page.  This is done
+// by passing the function name and arguments in attributes.
 GroovesharkProxy.sendAction = function(action, args) {
     GroovesharkProxyDiv.setAttribute('data-send-action', action);
     GroovesharkProxyDiv.setAttribute('data-send-action-arguments', JSON.stringify(args));
@@ -112,3 +126,4 @@ GroovesharkProxyDiv.addEventListener('GroovesharkSongEvent', function() {
     var data = JSON.parse(GroovesharkProxyDiv.innerText);
     GroovesharkProxy.songStatusCallback(data);
 });
+
